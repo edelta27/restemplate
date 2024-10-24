95ad707a48764c7697c841cf8eeeca72df71a02f
Edyta <edysiaborowska27@gmail.com>
2024-10-24 18:50:36 +0200

    adda @Value and file yml

--- src/main/java/com/example/restemplate/ShawnMendesProxy.java
+++ src/main/java/com/example/restemplate/ShawnMendesProxy.java
@@ -1,6 +1,7 @@
 package com.example.restemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
@@ -15,10 +16,14 @@ public class ShawnMendesProxy {
     // GET https://itunes.apple.com/search?term=shawnmendes&limit=1
     @Autowired
     RestTemplate restTemplate;
+    @Value("${shawnmendes.service.url}")
+    String url;
+    @Value("#{1+2}")
+    int suma;
 
     public String makeShawnMendesRequest(String term, Integer limit) {
 
-        String uri = "https://itunes.apple.com/search?term=" + term + "&limit=" + limit;
+        String uri = url + "/search?term=" + term + "&limit=" + limit;
         ResponseEntity<String> exchange = restTemplate.exchange(
                 uri,
                 HttpMethod.GET,
