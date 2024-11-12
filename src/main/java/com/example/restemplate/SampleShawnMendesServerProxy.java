b800f04c322598ebd49466b540de0d649723a5ed
Edyta <edysiaborowska27@gmail.com>
2024-11-12 18:49:36 +0100

    add Http Headers and Entity

--- src/main/java/com/example/restemplate/SampleShawnMendesServerProxy.java
+++ src/main/java/com/example/restemplate/SampleShawnMendesServerProxy.java
@@ -3,6 +3,8 @@ package com.example.restemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -24,18 +26,24 @@ public class SampleShawnMendesServerProxy {
     @Value("${sample-shawn-mendes-server.service.port}")
     int port;
 
-     public String makeRequest() {
+
+
+
+    public String makeRequest() {
         UriComponentsBuilder builder = UriComponentsBuilder
                 .newInstance()
                 .scheme("http")
                 .host(url)
                 .port(port)
                 .path("/shawn/songs");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("requestId", "asdasdydscsvdfydf");
+        HttpEntity<HttpHeaders> httpEntity = new HttpEntity<>(httpHeaders);
         try{
             ResponseEntity<String> response = restTemplate.exchange(
                     builder.build().toUri(),
                     HttpMethod.GET,
-                    null,
+                    httpEntity,
                     String.class
             );
             return response.getBody();
